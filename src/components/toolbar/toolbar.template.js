0d15a24d3094d4d294dfbf5cd0ce42bd9c57fb0e
Karyapkin Pavel <karyapkin@gmail.com>
2020-12-24 23:52:06 +0600

    fix: fix toolbar template

--- src/components/toolbar/toolbar.template.js
+++ src/components/toolbar/toolbar.template.js
@@ -2,7 +2,7 @@ import { actionTypes } from "../redux/actionTypes";
 
 function createBtn(config) {
   return `
-  <div class="btn ${config.active ? 'active' : ''}" data-format-text="${config.formatType}">  
+  <div class="btn" data-format-text="${config.formatType}">  
     <i class="material-icons">${config.iconName}</i>
   </div>
   `
@@ -12,32 +12,26 @@ const configBtns = [
   {
     iconName: 'format_align_left',
     formatType: actionTypes.setStyle.textAlignLeft,
-    active: false
   },
   {
     iconName: 'format_align_center',
     formatType: actionTypes.setStyle.textAlignCenter,
-    active: false,
   },
   {
     iconName: 'format_align_right',
     formatType: actionTypes.setStyle.textAlignRight,
-    active: false,
   },
   {
     iconName: 'format_bold',
     formatType: actionTypes.setStyle.textBold,
-    active: false,
   },
   {
     iconName: 'format_italic',
     formatType: actionTypes.setStyle.textItalic,
-    active: false,
   },
   {
     iconName: 'format_underline',
     formatType: actionTypes.setStyle.textUnderline,
-    active: false,
   },
 ];
 
