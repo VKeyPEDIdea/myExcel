1b6c10c7f15a6e386d731cbffe509418fb1cbe70
Karyapkin Pavel <karyapkin@gmail.com>
2020-12-24 22:34:00 +0600

    feat: add switch for buttons

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,4 +1,5 @@
 import { ExcelComponent } from "../../core/ExcelComponent";
+import { storage } from "../../core/utils";
 import { actionCreate } from "../redux/actionCreate";
 import { actionTypes } from "../redux/actionTypes";
 import { getTargetBtn, isActionBtn } from "./toolbar.helpers";
@@ -10,7 +11,10 @@ export class Toolbar extends ExcelComponent {
       ...options,
       listeners: ['mousedown'],
       subscribe: ['styleState'],
-		});
+    });
+
+    this.buttons = {};
+    this.styleState = this.store.getState().styleState;
   } 
 
   static getClassName() {
@@ -20,23 +24,72 @@ export class Toolbar extends ExcelComponent {
   init() {
     super.init();
 
-    // Получаем из table выделенную ячейку и ее адрес
     this.$on('table:select', cell => {
       this.cellSelection = cell.element.dataset.cellAddress;
       console.log('Toolbar:', this.cellSelection);
-		});
-	}
+      this.handleState(this.cellSelection);
+    });
+
+    this.findButtons();
+  }
+  
+  findButtons() {
+    Object.keys(actionTypes.setStyle).forEach(key => {
+      this.buttons[key] = this.root.element.querySelector(`[data-format-text="${key}"]`);
+    });
+  }
 	
 	storeChanged(changes) {
-		let currentCellStyles = changes.styleState[this.cellSelection];
-		this.switchBtnState(currentCellStyles);
+    this.styleState = changes.styleState;
+    
+		this.handleState(this.cellSelection);
 	}
 
-	switchBtnState(cellStyles) {
-		Object.keys(cellStyles).forEach(key => {
-			console.log(key, cellStyles[key]);
-		});
-	}
+	handleState(cellAddress) {
+    const cellStyles = this.styleState[cellAddress];
+
+    if (cellStyles) {
+      Object.keys(cellStyles).forEach(key => this.switchBtnState(key, cellStyles[key]));
+    } else {
+      this.resetBtn();
+    }
+  }
+
+  resetBtn() {
+    for (let key in this.buttons) {
+      this.buttons[key].classList.remove('active');
+    }
+
+    this.buttons.textAlignLeft.classList.add('active');
+  }
+  
+  switchBtnState(key, value) {
+    switch (key) {
+      case 'bold':
+        if (value == undefined) this.buttons.textBold.classList.add('active');
+        
+        value
+          ? this.buttons.textBold.classList.add('active')
+          : this.buttons.textBold.classList.remove('active');            
+        break;
+      case 'italic':
+        if (value == undefined) this.buttons.textItalic.classList.add('active');
+
+        value
+          ? this.buttons.textItalic.classList.add('active')
+          : this.buttons.textItalic.classList.remove('active');
+        break;
+      case 'underline':
+        if (value == undefined) this.buttons.textUnderline.classList.add('active');
+
+        value
+          ? this.buttons.textUnderline.classList.add('active')
+          : this.buttons.textUnderline.classList.remove('active');
+        break;
+      default:
+        break;
+    }
+  }
 
   onMousedown(event) {
     if (isActionBtn(event)) {
